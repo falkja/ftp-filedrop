61b3b47a8b3a12937024468e9563ed144a6c959d
@nolim1t <barry.teoh+github@gmail.com>
2012-09-14 15:40:44 +1000

    Add destfile environment variable support. This allows more control over what the file ends up as.

--- drop.rb
+++ drop.rb
@@ -10,18 +10,28 @@ require 'net/ftp'
 require 'guid'
 
 if server != nil then
+    if ENV['destfile'] == nil
+        destfile = ''
+    elsif ENV['destfile'] != nil
+        destfile = ENV['destfile']
+    else
+        destfile = ''
+    end
     ftp = Net::FTP.new(server)
     ftp.login(user,pass)
     if file != 'listonly' then
         # Randomize the target name a bit 
         fileext = file.split('.')[file.split('.').length - 1]
         thefilename = "#{Guid.new.to_s.gsub('-','')[0, 8]}." + fileext
-        puts "Sending #{file} as #{thefilename}"
     end
     if dir != nil then
         ftp.chdir(dir)
     end
     if thefilename != nil
+        if destfile != ''
+            thefilename = destfile
+        end
+        puts "Sending #{file} as #{thefilename}"        
         ftp.putbinaryfile(file, thefilename)
     end
     ftp.list.each {|file|
